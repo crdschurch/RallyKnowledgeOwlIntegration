a45bfa1eb8e6196fd750d8271d3badf57e07ae1f
Charlie Retzler <charlie.retzler@ingagepartners.com>
2016-04-12 17:19:31 -0400

    US3387: Created shell for Rally to KO Integration

---
+++ RallyKnowledgeOwlIntegration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Rally.RestApi;
+using Rally.RestApi.Response;
+
+namespace RallyKnowledgeOwlIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rally = new RallyDataService();
+            var result = rally.LoadStoriesAndDefects();
+
+        }
+    }
+}
---
+++ RallyKnowledgeOwlIntegration/RallyDataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Rally.RestApi;
+using Rally.RestApi.Response;
+
+namespace RallyKnowledgeOwlIntegration
+{
+    public class RallyDataService
+    {
+        public QueryResult LoadStoriesAndDefects()
+        {
+            string apiKey = Environment.GetEnvironmentVariable("RALLY_API_KEY");
+            string serverUrl = "https://rally1.rallydev.com";
+
+            var restApi = new RallyRestApi();
+            var authenticationResult = restApi.AuthenticateWithApiKey(apiKey, serverUrl);
+
+            //Query for items
+            var request = new Request("defect");
+            request.Fetch = new List<string>() { "Name", "Description", "FormattedID" };
+            request.Query = new Query("Name", Query.Operator.DoesNotContain, "My Defect");
+
+            var queryResult = restApi.Query(request);
+            return queryResult;
+        }
+    }
+}
